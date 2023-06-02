7fbccb1f3cfb893fc2210d431b1b02b6a0e28027
abjudge <abdelrahmanelkady0@gmail.com>
2023-06-02 12:10:15 +0300

    added delete function

--- trainee/views.py
+++ trainee/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 # Create your views here.
+from .models import *
+from course import *
 def traineelist(request):
     if( 'username' in request.session):
-        return render(request,'trainee/list.html')
+        trainees=Traineee.objects.all()
+        context={}
+        context["trainees"]=trainees
+        return render(request,'trainee/list.html',context)
     else:
         return HttpResponseRedirect('/')
 
 def traineeadd(request):
 
     if( 'username' in request.session):
-        return render(request,'trainee/add.html')
+        context= {}
+        context['courses']=Course.objects.all()
+        if (request.method=='POST'):
+            cour=Course.objects.get(id=request.POST['coursename'])
+            Traineee.objects.create(name=request.POST['traineename'],courseid=cour)
+
+        return render(request,'trainee/add.html',context)
     else:
         return HttpResponseRedirect('/')
 
@@ -23,8 +34,8 @@ def traineeupdate(request,id):
 
 
 def traineedelete(request, ID):
-
+    Traineee.objects.filter(id=ID).delete()
     if ('username' in request.session):
-        return HttpResponse("Trainee "+str(ID)+"deleted")
+        return HttpResponseRedirect('/Trainee')
     else:
         return HttpResponseRedirect('/')
